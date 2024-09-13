744faaaab4c0cf30b753b959569a4c9270a646ff
Usuario <zvasquezvalenzuela@gmail.com>
2024-09-12 21:03:51 -0500

    add commit

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -23,12 +23,157 @@ class Reminder:
 # TODO: Implement Event class here
 @dataclass
 class Event:
-    def __init__(self, title: str, description: str, date_: date, start_at: time, end_at: time):
+    def __init__(self, title: str, description: str, date_: date, start_at: time, end_at: time, id: str = None):
         self.title: str = title
         self.description: str = description
         self.date_: date = date_
         self.start_at: time = start_at
         self.end_at: time = end_at
+        self.reminders: list[Reminder] = []
+        self.id: str = generate_unique_id()
+
+    def add_reminder(self, date_time: datetime, reminder_type: str = Reminder.EMAIL):
+        reminder = Reminder(date_time=date_time, type=reminder_type)
+        self.reminders.append(reminder)
+
+    def delete_reminder(self, reminder_index: int):
+        if 0 <= reminder_index < len(self.reminders):
+            del self.reminders[reminder_index]
+        else:
+            reminder_not_found_error()
+
+    def __str__(self) -> str:
+        return f'ID: {self.id} Event title: {self.title} Description: {self.description} Time: {self.start_at} - {self.end_at}'
+
+
+# TODO: Implement Day class here
+class Day:
+    def __init__(self, date_: date):
+        self.date_: date = date_
+        self.slots: dict[time, str | None] = {}
+
+    def _init_slots(self):
+        pass
+
+    def add_event(self, event_id: str, start_at: time, end_at: time):
+        current_time = start_at
+        while current_time < end_at:
+            if self.slots.get(current_time) is not None:
+                slot_not_available_error()
+                return
+            current_time = self._increment_time(current_time)
+
+        current_time = start_at
+        while current_time < end_at:
+            self.slots[current_time] = event_id
+            current_time = self._increment_time(current_time)
+
+    def delete_event(self, event_id: str):
+        deleted = False
+        for slot, saved_id in self.slots.items():
+            if saved_id == event_id:
+                self.slots[slot] = None
+                deleted = True
+        if not deleted:
+            event_not_found_error()
+
+    def update_event(self, event_id: str, start_at: time, end_at: time):
+        for slot in self.slots:
+            if self.slots[slot] == event_id:
+                self.slots[slot] = None
+
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                if self.slots[slot]:
+                    slot_not_available_error()
+                else:
+                    self.slots[slot] = event_id
+
+    def _increment_time(self, t: time) -> time:
+        new_minute = t.minute + 15
+        new_hour = t.hour
+        if new_minute == 60:
+            new_minute = 0
+            new_hour += 1
+        return time(new_hour, new_minute)
+
+
+# TODO: Implement Calendar class here
+class Calendar:
+    def __init__(self):
+        self.events: dict[str, Event] = {}
+
+    def add_event(self, title: str, description: str, date_: date, start_at: time, end_at: time):
+        pass
+
+    def add_reminder(self, event_id: str, date_time: datetime, type_: str):
+        pass
+
+    def find_available_slots(self, date_: date) -> list[time]:
+        pass
+
+    def update_event(self, event_id: str, title: str, description: str, date_: date, start_at: time, end_at: time):
+        event = self.events[event_id]
+        if not event:
+            event_not_found_error()
+
+        is_new_date = False
+
+        if event.date_ != date_:
+            self.delete_event(event_id)
+            event = Event(title=title, description=description, date_=date_, start_at=start_at, end_at=end_at)
+            event.id = event_id
+            self.events[event_id] = event
+            is_new_date = True
+            if date_ not in self.days:
+                self.days[date_] = Day(date_)
+            day = self.days[date_]
+            day.add_event(event_id, start_at, end_at)
+        else:
+            event.title = title
+            event.description = description
+            event.date_ = date_
+            event.start_at = start_at
+            event.end_at = end_at
+
+        for day in self.days.values():
+            if not is_new_date and event_id in day.slots.values():
+                day.delete_event(event.id)
+                day.update_event(event.id, start_at, end_at)
+
+    def delete_event(self, event_id: str):
+        if event_id not in self.events:
+            event_not_found_error()
+
+        self.events.pop(event_id)
+
+        for day in self.days.values():
+            if event_id in day.slots.values():
+                day.delete_event(event_id)
+                break
+
+    def find_events(self, start_at: date, end_at: date) -> dict[date, list[Event]]:
+        events: dict[date, list[Event]] = {}
+        for event in self.events.values():
+            if start_at <= event.date_ <= end_at:
+                if event.date_ not in events:
+                    events[event.date_] = []
+                events[event.date_].append(event)
+        return events
+
+    def delete_reminder(self, event_id: str, reminder_index: int):
+        event = self.events.get(event_id)
+        if not event:
+            event_not_found_error()
+
+        event.delete_reminder(reminder_index)
+
+    def list_reminders(self, event_id: str) -> list[Reminder]:
+        event = self.events.get(event_id)
+        if not event:
+            event_not_found_error()
+
+        return event.reminders
 
 
 # TODO: Implement Day class here
